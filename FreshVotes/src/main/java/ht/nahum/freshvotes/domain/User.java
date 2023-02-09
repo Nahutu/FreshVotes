c5f1b235e85450aaa6c8cc6c73c96ed59c98cccb
Nahutu <jnahum.constant@gmail.com>
2023-02-08 21:52:38 -0600

    Logging into FreshVotes

--- FreshVotes/src/main/java/ht/nahum/freshvotes/domain/User.java
+++ FreshVotes/src/main/java/ht/nahum/freshvotes/domain/User.java
@@ -1,9 +1,16 @@
 package ht.nahum.freshvotes.domain;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import ht.nahum.freshvotes.security.Authority;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -15,6 +22,16 @@ public class User {
 	private String username;
 	private String password;
 	private String name;
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "user")
+	Set<Authority> authorities = new HashSet<>();
+	@OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER, mappedBy = "user")
+	Set<Product> products = new HashSet<>();
+	
+	
+	public User() {
+		
+	}
+	
 	public Long getId() {
 		return id;
 	}
@@ -39,5 +56,27 @@ public class User {
 	public void setName(String name) {
 		this.name = name;
 	}
+	public Set<Authority> getAuthorities() {
+		return authorities;
+	}
+	public void setAuthorities(Set<Authority> authorities) {
+		this.authorities = authorities;
+	}
+	
+	public Set<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(Set<Product> products) {
+		this.products = products;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", name=" + name
+				+ ", authorities=" + authorities + "]";
+	}
+	
+	
 
 }
